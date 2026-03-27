d18ec2fb370c3b797e55e122fa52076983f4685a
Rykarrddo <ryk123321gmail.com>
2026-03-27 05:02:58 -0600

    Fondo de pantalla, cambio de estilo y nodos brillantes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  // Fuerza a Vite a permitir archivos de node_modules y de la raíz
+  vite: {
+    server: {
+      fs: {
+        allow: ['/home/rykardo/', './'] 
+      },
+      cors: true // Esto debería mitigar el bloqueo del cross-origin de la imagen
+    }
+  }
 });
